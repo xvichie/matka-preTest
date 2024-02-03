44d3c6f5c885b0396dcfa5514126c5055c82973d
xvichie <georgiandodo.dodoshka@gmail.com>
2024-02-03 22:18:08 +0400

    Added AdPLaceholder Links

--- frontend/src/components/AdPlaceholderComponent/AdPlaceholderComponent.jsx
+++ frontend/src/components/AdPlaceholderComponent/AdPlaceholderComponent.jsx
@@ -5,7 +5,7 @@ function AdPlaceholderComponent({AdId}) {
   
   return (
     <div className='AdPlaceholderComponent'>
-      <a href="/">
+      <a target='_blank' href="https://docs.google.com/forms/d/1zpduQEM3Xm-leGwVJRgCfNsef8Da4982DuJLA0LG2cs/edit?usp=sharing_eil_se_dm&ts=65bccb87">
         <div className="Ad-Ad">
             სარეკლამო ადგილი #{AdId}
         </div>
